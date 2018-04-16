88061d6c4ba19c6bbf7e2e9638fb7fa06da60c0d
Goran Vukovic <therealvuko@gmx.de>
2018-04-16 23:28:06 +0200

    Update implemented

---
+++ ITF_WPF-CRUD/Insert.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ITF_WPF_CRUD
+{
+    /// <summary>
+    /// Interaktionslogik für Insert.xaml
+    /// </summary>
+    public partial class Insert : Window
+    {
+        loginEntities dataEntities = new loginEntities();
+
+        public Insert()
+        {
+            InitializeComponent();
+        }
+
+        private void insertBtn_Click(object sender, RoutedEventArgs e)
+        {
+            userMask newUser = new userMask()
+            {
+                id = dataEntities.userMasks.Count() + 1,
+                username = text_username.Text,
+                password = text_password.Text
+            };
+
+            dataEntities.userMasks.Add(newUser);
+            dataEntities.SaveChanges();
+            dataEntities.SaveChangesAsync();
+            this.Hide();
+        }
+    }
+}
---
+++ ITF_WPF-CRUD/Update.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ITF_WPF_CRUD
+{
+    /// <summary>
+    /// Interaktionslogik für Update.xaml
+    /// </summary>
+    public partial class Update : Window
+    {
+        loginEntities dataEntities = new loginEntities();
+        int Id;
+
+        public Update()
+        {
+            InitializeComponent();
+        }
+
+        public Update(int memberID)
+        {
+            this.Id = memberID;
+            InitializeComponent();
+            text_ID.Text = this.Id.ToString();
+            text_ID.IsEnabled = false;
+        }
+
+        private void updateBtn_Click(object sender, RoutedEventArgs e)
+        {
+            //int x = Convert.ToInt32(text_ID.Text);
+            userMask updateMember = (from m in dataEntities.userMasks
+                                     where m.id == Id
+                                     select m).Single();
+            updateMember.username = text_username.Text;
+            updateMember.password = text_password.Text;
+            dataEntities.SaveChanges();
+
+            //.ItemsSource = _db.members.ToList();
+            this.Hide();
+
+        }
+
+    }
+}
