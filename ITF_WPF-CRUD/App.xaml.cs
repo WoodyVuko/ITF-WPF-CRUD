26b4cbfaa239d93a8ad7b2775c4bf64f689ad4cf
Goran Vukovic <therealvuko@gmx.de>
2018-04-16 21:54:54 +0200

    Clean

---
+++ ITF_WPF-CRUD/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ITF_WPF_CRUD
+{
+    /// <summary>
+    /// Interaktionslogik für "App.xaml"
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
